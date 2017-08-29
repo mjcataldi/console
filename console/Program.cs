931ea163df2a43b9b1a61f80492cb8cfdd7334f7
Matthew Cataldi <mcataloe@gmail.com>
2017-08-28 21:30:57 -0500

    Initialized files for console log.

---
+++ console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World! How are you?");
+            string response = ReadLine();
+
+            // string[] ResponseArray = response.Split();
+
+            WriteLine($"You responded '{ response }', so I hope that means you're ok!");
+
+        }
+    }
+}
